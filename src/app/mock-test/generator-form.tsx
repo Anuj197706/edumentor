971e6470cd85cde4798fd1819d2fe34bb078ce8c
Anuj197706 <anuj03102006@gmail.com>
2025-09-06 18:59:39 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/app/mock-test/generator-form.tsx
+++ src/app/mock-test/generator-form.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import type { Subject, Chapter } from '@/lib/data';
 import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
